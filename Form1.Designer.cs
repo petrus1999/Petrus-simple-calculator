971d0d354e36716871837a68bbc28697e1af55b0
petrus <pshai671@gmail.com>
2022-11-01 09:37:44 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Petrus_simple_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculator = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Calculator
+            // 
+            this.Calculator.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Calculator.Location = new System.Drawing.Point(202, 127);
+            this.Calculator.Multiline = true;
+            this.Calculator.Name = "Calculator";
+            this.Calculator.Size = new System.Drawing.Size(235, 38);
+            this.Calculator.TabIndex = 0;
+            this.Calculator.Text = "PETRUS  SIMPLE  CALCULATOR ";
+            this.Calculator.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(111, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CLICK HERE TO GO TO OUR SIMPLE CALCULATOR APP";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Petrus_simple_calculator.Properties.Resources.BACK_IMAGE;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Calculator);
+            this.Name = "Form1";
+            this.Text = "9";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Calculator;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Petrus_simple_calculator
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        float num1, ans;
+        int count;
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            count = 0;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 4;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 2;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0 + 0;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            int c = textBox1.TextLength;
+            int flag = 0;
+            string text = textBox1.Text;
+            for (int i = 0; i < c; i++)
+            {
+                if (text[i].ToString() == ".")
+                {
+                    flag = 1; break;
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+            if (flag == 0)
+            {
+                textBox1.Text = textBox1.Text + ".";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 3;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            compute(count);
+        }
+        public void compute(int count)
+        {
+            switch (count)
+            {
+                case 1:
+                    ans = num1 - float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+                case 2:
+                    ans = num1 + float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+                case 3:
+                    ans = num1 * float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+                case 4:
+                    ans = num1 / float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                num1 = float.Parse(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+                count = 1;
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+            Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+    }
+}
